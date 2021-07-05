04e5fddb9a4705d444ea71507f6fad3a67e4087d
nksoluna <xitnikz@gmail.com>
2021-07-05 19:02:19 +0700

    insert method

--- bin/digital.dart
+++ bin/digital.dart
@@ -1,5 +1,23 @@
 import 'package:digital/digital.dart' as digital;
 
+class Clock {
+  int hh;
+  int mm;
+
+  Clock(this.hh, this.mm);
+
+  void turnOn() {
+    hh = 0;
+    mm = 0;
+  }
+
+  void showtime() {
+    print('the time is [$hh : $mm]');
+  }
+
+  void setTime() {}
+}
+
 void main(List<String> arguments) {
   print('Hello world: ${digital.calculate()}!');
 }
