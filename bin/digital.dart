dd84994331b737bbc0f081f6718ad753b2b92fa0
nksoluna <xitnikz@gmail.com>
2021-07-05 20:28:04 +0700

    90%

--- bin/digital.dart
+++ bin/digital.dart
@@ -28,26 +28,36 @@ class Clock {
 void main(List<String> arguments) {
   final clocks = Clock(0, 0);
   String? command = stdin.readLineSync();
+  String? commandnumber = stdin.readLineSync() ?? "nothing in here";
+  List commandList = commandnumber.split(' ');
+  var textcommand = commandList[0].toString();
+  var hh = commandList[0].toString();
+  var hour = int.parse(hh);
+  var mm = commandList[1].toString();
+  var minutes = int.parse(mm);
 
   do {
     if (command == 'On') {
-      clocks.turnOn(18, 0);
+      clocks.turnOn(hour, minutes);
       clocks.showtime();
     } else if (command == 'Set') {
       do {
         command = stdin.readLineSync();
         if (command == 'inc') {
           clocks.inchour();
+        } else if (command == 'Set') {
           clocks.showtime();
-        } else if (command != 'Set') {
           break;
+        } else {
+          print('Wrong command');
         }
       } while (command != 'Set');
     } else if (command == 'inc') {
       clocks.incmin();
-      clocks.showtime();
     } else if (command == 'Show') {
       clocks.showtime();
+    } else {
+      print('Wrong command');
     }
     command = stdin.readLineSync();
   } while (command != 'Exit');
